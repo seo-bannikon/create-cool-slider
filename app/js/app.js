329247780849c938ab15e05f699ce72e96c1b697
Владислав <102479085+seo-bannikon@users.noreply.github.com>
2023-03-27 17:05:44 +0300

    added sliders

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,23 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import { Swiper, Parallax, Mousewheel, Controller } from "swiper"
+Swiper.use([ Parallax, Mousewheel, Controller ])
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	const swiperIMG = new Swiper('.slider-img', {
+		loop: false,
+		speed: 2400,
+		parallax: true
+	})
+
+	const swiperText = new Swiper('.slider-text', {
+		loop: false,
+		speed: 2400,
+		mousewheel: {
+			invert: false
+		}
+	})
+
+	swiperIMG.controller.control = swiperText
+	swiperText.controller.control = swiperIMG
 
 })
