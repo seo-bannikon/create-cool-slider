3ad8cd1238be0f8205fb8d91ad38575dff4b7999
Владислав <102479085+seo-bannikon@users.noreply.github.com>
2023-04-02 19:31:15 +0300

    added modal and other

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,35 @@
-import { Swiper, Parallax, Mousewheel, Controller } from "swiper"
-Swiper.use([ Parallax, Mousewheel, Controller ])
+import { Swiper, Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation } from "swiper"
+Swiper.use([ Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation ])
+
+import {gsap, Power2} from 'gsap'
+
+import MicroModal from "micromodal"
 
 document.addEventListener('DOMContentLoaded', () => {
+
+	// Modal
+
+	MicroModal.init({
+		openTrigger: 'data-micromodal-open',
+		closeTrigger: 'data-micromodal-close',
+		disableFocus: true,
+		disableScroll: true,
+		awaitOpenAnimation: true,
+		awaitCloseAnimation: true
+	})
 
 	const swiperIMG = new Swiper('.slider-img', {
 		loop: false,
 		speed: 2400,
-		parallax: true
+		parallax: true,
+		pagination: {
+			el: '.slider-pagination-count .total',
+			type: 'custom',
+			renderCustom: function(swiper, current, total) {
+				let totalRes = total >= 10 ? total : `0${total}`
+				return totalRes
+			}
+		}
 	})
 
 	const swiperText = new Swiper('.slider-text', {
@@ -14,10 +37,68 @@ document.addEventListener('DOMContentLoaded', () => {
 		speed: 2400,
 		mousewheel: {
 			invert: false
+		},
+		pagination: {
+			el: '.slider-pagination',
+			clickable: true
+		},
+		scrollbar: {
+			el: '.slider-scrollbar',
+			draggable: true
+		},
+		navigation: {
+			prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next'
 		}
 	})
 
 	swiperIMG.controller.control = swiperText
 	swiperText.controller.control = swiperIMG
 
+	let gear = document.querySelector('.slider-gear')
+
+	swiperText.on('slideNextTransitionStart', function() {
+		gsap.to(gear, 2.8, {
+			rotation: '+=40',
+			ease: Power2.easeOut
+		})
+	})
+
+	swiperText.on('slidePrevTransitionStart', function() {
+		gsap.to(gear, 2.8, {
+			rotation: '-=40',
+			ease: Power2.easeOut
+		})
+	})
+
+	// Slide change
+
+	let curnum = document.querySelector('.slider-pagination-count .current'),
+		pagcur = document.querySelector('.slider-pagination-current__num')
+
+	swiperText.on('slideChange', function() {
+		let ind    = swiperText.realIndex + 1,
+			indRes = ind >= 10 ? ind :`0${ind}`
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: -10,
+			opacity: 0,
+			ease: Power2.easeOut,
+			onComplete: function() {
+				gsap.to(curnum, .1, {
+					force3D: true,
+					y: 10
+				})
+				curnum.innerHTML = indRes
+				pagcur.innerHTML = indRes
+			}
+		})
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: 0,
+			opacity: 1,
+			ease: Power2.easeOut,
+			delay: .3
+		})
+	})
 })
