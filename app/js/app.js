27f7b36ee8edc66fca16773c8ad6b49242a03f88
Владислав <102479085+seo-bannikon@users.noreply.github.com>
2023-04-06 20:41:46 +0300

    ready html, js and css

--- app/js/app.js
+++ app/js/app.js
@@ -101,4 +101,49 @@ document.addEventListener('DOMContentLoaded', () => {
 			delay: .3
 		})
 	})
+
+	//cursor
+
+	const body   = document.querySelector('body'),
+		  cursor = document.getElementById('cursor'),
+		  links  = document.getElementsByTagName('a')
+
+	let mouseX = 0, mouseY = 0, posX = 0, posY = 0
+
+	function mouseCoords(e) {
+		mouseX = e.pageX
+		mouseY = e.pageY
+	}
+
+	gsap.to({}, .01, {
+		repeat: -1,
+		onRepeat: () => {
+			posX += (mouseX - posX) / 6
+			posY += (mouseY - posY) / 6
+			gsap.set(cursor, {
+				css: {
+					left: posX,
+					top: posY
+				}
+			})
+		}
+	})
+
+	for(let i = 0; i < links.length; i++) {
+		links[i].addEventListener('mouseover', () => {
+			cursor.classList.add('active')
+		})
+		links[i].addEventListener('mouseout', () => {
+			cursor.classList.remove('active')
+		})
+	}
+
+	body.addEventListener('mousemove', e => {
+		mouseCoords(e)
+		cursor.classList.remove('hidden')
+	})
+
+	body.addEventListener('mouseout', e => {
+		cursor.classList.add('hidden')
+	})
 })
